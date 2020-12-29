9693b6f725f6b363c858f3fec2c2b737163f0b9c
Christian Alvarez <>
2020-12-29 14:11:09 +0800

    Cleanup comment

--- EatList/Networking/NetworkRequestManager.swift
+++ EatList/Networking/NetworkRequestManager.swift
@@ -13,8 +13,11 @@ typealias RequestSuccessBlock = (Response) -> Void
 typealias RequestFailedBlock = (EatListError) -> Void
 
 class NetworkRequestManager {
-    /// For centralized network fetch error handling.
-    /// Should we wish to add other possible errors in the future that all other APIs will likely have, e.g., server maintenance, we put the handling here.
+    /**
+     For centralized network fetch error handling.
+     Should we wish to add other possible errors in the future that all other APIs will likely have,
+     e.g., server maintenance, we put the handling here.
+     */
     static func fetchData<T: TargetType>(
         target: T,
         provider: MoyaProvider<T>,
