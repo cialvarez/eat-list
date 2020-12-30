1b4a2caca9b88b5b6a1cdc2f640c169b8a19120b
Christian Alvarez <>
2020-12-30 16:37:01 +0800

    Add unit test

---
+++ EatListTests/EatListTests.swift
@@ -0,0 +1,32 @@
+//
+//  EatListTests.swift
+//  EatListTests
+//
+//  Created by Christian Alvarez on 12/30/20.
+//
+
+import XCTest
+
+class EatListTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
