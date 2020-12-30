e2482f6330624e41a581da616671132e7f637f39
Christian Alvarez <>
2020-12-30 16:45:11 +0800

    Add debug logging service and target details view model test

---
+++ EatList/Services/DebugLoggingService.swift
@@ -0,0 +1,59 @@
+//
+//  DebugLoggingService.swift
+//  EatList
+//
+//  Created by Christian Alvarez on 12/30/20.
+//
+
+import Foundation
+import UIKit
+
+enum DebugLogType: String {
+    /// General information, e.g., lifecycle method calls
+    case info
+    /// For Swift-side errors or problems
+    case error
+    /// For API/Server-side problems, e.g, unexpected response
+    case warning
+    /// For successful API calls
+    case success
+    /// For failed API calls
+    case failure
+    
+    var symbol: String {
+        switch self {
+        case .info: return "🔵"
+        case .error: return "⛔️"
+        case .warning: return "⚠️"
+        case .success: return "✅"
+        case .failure: return "❌"
+        }
+    }
+}
+
+class DebugLog {
+    private init() { }
+    
+    class func debugMessage(status: DebugLogType,
+                            message: String = "",
+                            file: String = #file,
+                            function: String = #function,
+                            line: Int = #line) {
+        
+        var logText = ""
+        
+        let fileName =
+            file.components(separatedBy: "/")
+            .last?
+            .replacingOccurrences(of: ".swift", with: "")
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "HH:mm:ss.SSS"
+        logText = "\(dateFormatter.string(from: Date())) \(status.symbol) \(status.rawValue.uppercased()) \(fileName ?? file).\(function):\(line)"
+        
+        logText.append(message.isEmpty ? "" : " - \(message)")
+        
+        #if DEBUG
+        print(logText)
+        #endif
+    }
+}
---
+++ EatListTests/TargetDetailsVMTests.swift
@@ -0,0 +1,32 @@
+//
+//  TargetDetailsVMTests.swift
+//  EatListTests
+//
+//  Created by Christian Alvarez on 12/30/20.
+//
+
+import XCTest
+@testable import EatList
+class TargetDetailsVMTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testViewModel() throws {
+        let viewModel = TargetDetailsViewModel()
+//        viewModel.transform(input: .init(restaurantDetails: <#T##RestaurantDetails#>))
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
