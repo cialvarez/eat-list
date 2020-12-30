d0d5bdf00e63d52f409f893189fad66df5369e19
Christian Alvarez <>
2020-12-30 20:24:52 +0800

    - Disable dark mode - Fix issue wherein devices above iOS 13 are shown a blank screen

--- EatList/SceneDelegate.swift
+++ EatList/SceneDelegate.swift
@@ -6,7 +6,38 @@
 //
 
 import UIKit
+import Unrealm
 
+@available(iOS 13.0, *)
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
+    var coordinator: MainCoordinator?
+    
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowsScene = scene as? UIWindowScene else {
+            return
+        }
+        
+        let navigationController = UINavigationController()
+        setupCoordinator(with: navigationController)
+        setupWindow(with: navigationController, and: windowsScene)
+        setupRealm()
+    }
+    
+    private func setupCoordinator(with navigationController: UINavigationController) {
+        coordinator = MainCoordinator(navigationController: navigationController)
+        coordinator?.start()
+    }
+    
+    private func setupWindow(with rootViewController: UIViewController,
+                             and windowScene: UIWindowScene) {
+        window = UIWindow(windowScene: windowScene)
+        window?.rootViewController = rootViewController
+        window?.makeKeyAndVisible()
+    }
+    
+    private func setupRealm() {
+        Realm.registerRealmables(Restaurant.self)
+    }
+
 }
